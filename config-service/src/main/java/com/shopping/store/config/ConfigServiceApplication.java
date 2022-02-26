466a5b16cc079ab4eac1c4bd8cf38fc2ccba6a17
Diego Martinez Giardini <diego.martinez.giardini@gmail.com>
2022-02-26 20:50:30 +0100

    Config-service updated. Bootstrap.yml changed

--- config-service/src/main/java/com/shopping/store/config/ConfigServiceApplication.java
+++ config-service/src/main/java/com/shopping/store/config/ConfigServiceApplication.java
@@ -2,10 +2,11 @@ package com.shopping.store.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
