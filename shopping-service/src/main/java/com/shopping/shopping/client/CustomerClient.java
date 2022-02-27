5df69966c4da80572ae94dbbfabdcdbc66174b46
Diego Martinez Giardini <diego.martinez.giardini@gmail.com>
2022-02-27 16:22:04 +0100

    Gateway service config added

--- shopping-service/src/main/java/com/shopping/shopping/client/CustomerClient.java
+++ shopping-service/src/main/java/com/shopping/shopping/client/CustomerClient.java
@@ -1,12 +1,14 @@
 package com.shopping.shopping.client;
 
 import com.shopping.shopping.model.Customer;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "customer-service", fallback = CustomerResilience4JFallback.class)
+@FeignClient(name = "customer-service")
+@CircuitBreaker(name = "customer-service", fallbackMethod = "null")
 public interface CustomerClient {
 
     @GetMapping("api/customers/{id}")
